525c656cc1016d4a3f047b1d30cfd69e2cd1768b
zhangguangyong <1243610991@qq.com>
2019-06-25 11:25:53 +0800

    spring framework 2.5.6 环境搭建

---
+++ v256/src/main/java/study/springframework/v256/HelloWorld.java
@@ -0,0 +1,13 @@
+package study.springframework.v256;
+
+public class HelloWorld {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
